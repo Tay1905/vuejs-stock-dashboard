55fff4df03d8151d6823cf8627420ba573993799
Tay1905 <137186217+Tay1905@users.noreply.github.com>
2024-07-24 23:02:44 +0200

    background

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="div">
+
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -17,10 +17,14 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: linear-gradient(rgb(17, 17, 88),rgb(6, 9, 36));
+
+}
+#div{
+  height: 100vh;
+  width: 100vw;
+}
+body{
+  margin: 0;
 }
 </style>
