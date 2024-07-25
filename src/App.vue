471055af35ba0cdfff8148de77498e6f8083b43c
Tay1905 <137186217+Tay1905@users.noreply.github.com>
2024-07-26 00:03:30 +0200

    first component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <div id="div">
-
+  <div id="app">
+    <BaseCard>
+      <h1>hello WSworld</h1>
+    </BaseCard>
   </div>
-
 </template>
 
 <script>
+import BaseCard from './components/BaseCard.vue';
+
+
+
+
 
 export default {
   name: 'App',
   components: {
+    BaseCard
   }
 }
 </script>
@@ -17,14 +24,15 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  background: linear-gradient(rgb(17, 17, 88),rgb(6, 9, 36));
 
 }
-#div{
-  height: 100vh;
-  width: 100vw;
-}
-body{
+
+body {
   margin: 0;
+  padding: 150px;
+  background: radial-gradient(71.1% 100% at 50% 0%, #020204, #011F35 100%);
+box-sizing: border-box;
+height: 100vh;
+width: 100vw;
 }
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card">
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script scoped>
+  export default {
+    name: 'BaseCard'
+    
+  }
+  </script>
+  
+  <style scoped>
+  .card {
+    background-color: #011F35;
+    border-radius: 16px;
+    padding: 24px 32px;
+    width: 100%;
+  }
+  </style>
+  
